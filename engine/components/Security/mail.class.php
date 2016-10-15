cbf7b5dd469dec96301ac2b5b6b8a194772e436e
alexsoftinc <norkin-91@list.ru>
2016-10-15 19:18:37 +0500

    Create mail.class.php

---
+++ engine/components/Security/mail.class.php
@@ -0,0 +1,22 @@
+<?php
+class Mail
+{
+    public $tos     = ''; # Кому отправляется письмо
+    public $subject = ''; # Тема письма
+    public $headers = ''; # Заголовки должны быть отправлены
+    public $message = ''; # Сообщение письма которое будет генерироватся при его отправке в зависимости от нужл
+
+    public function headers()
+    {
+        $this->headers .= 'From: admin@project.ru' . "\r\n" .
+        'Reply-To: admin@project.ru' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+        return $this->headers;
+    }
+
+    public function send()
+    {
+        mail($this->tos, $this->subject, $this->message, $this->headers());
+    }
+}
