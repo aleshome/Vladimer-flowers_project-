df871f583f24904e475b4d0fde8e2c08b0b91458
alexsoftinc <norkin-91@list.ru>
2016-10-15 19:30:05 +0500

    Create ReviewsController.php

---
+++ controllers/ReviewsController.php
@@ -0,0 +1,10 @@
+<?php
+class ReviewsController
+{
+    function actionIndex()
+    {
+        echo '<h2>Сдесь будет информация о том как нас любят и ценят все открыто из социальных сетей, Но заказа не было</h2>';
+        
+        return true;
+    }
+}
